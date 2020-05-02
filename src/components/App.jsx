83869ae6b4426c9161e33f01a8693432e8300a80
Alexanderqwer <llkkfi27456@gmail.com>
2020-05-02 22:56:40 +0300

    added MonthSummary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-===/** @format */
+/** @format */
 
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
 
-import ProjectTitle from './ProjectTitle/ProjectTitle.jsx';
-import AuthInput from './AuthInput/AuthInput';
+import MonthSummary from './MonthSummary/Index';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MonthSummary
+        data={[
+          { mounth: 'Jun', cost: 300, id: 'Index1' },
+          { mounth: 'Feb', cost: 400, id: 'Index2' },
+          { mounth: 'Ojest', cost: 100, id: 'Index3' },
+          { mounth: 'Jun', cost: 300, id: '4' },
+          { mounth: 'Feb', cost: 400, id: 'Index25' },
+          { mounth: 'Ojest', cost: 100, id: 'Index63' },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default App;
