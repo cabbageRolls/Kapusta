927a0501e279cbe3a05accae102c97793f544470
Saashen <saashen.kyiv@gmail.com>
2020-05-02 10:26:37 +0300

    ActionButton done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import '../CSS/index.css';
 
 import ProjectTitle from './ProjectTitle/ProjectTitle.jsx';
 import AuthInput from './AuthInput/AuthInput';
+import ActionButton from './ActionButton/ActionButton';
 
 function App() {
   return <div></div>;
