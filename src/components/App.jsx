141b86c4259c5b181b1289a6e03a7541fe1d56c2
Alexanderqwer <llkkfi27456@gmail.com>
2020-05-02 22:59:32 +0300

    clean app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,8 @@ import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
 
-import MonthSummary from './MonthSummary/Index';
-
 function App() {
-  return (
-    <div>
-      <MonthSummary
-        data={[
-          { mounth: 'Jun', cost: 300, id: 'Index1' },
-          { mounth: 'Feb', cost: 400, id: 'Index2' },
-          { mounth: 'Ojest', cost: 100, id: 'Index3' },
-          { mounth: 'Jun', cost: 300, id: '4' },
-          { mounth: 'Feb', cost: 400, id: 'Index25' },
-          { mounth: 'Ojest', cost: 100, id: 'Index63' },
-        ]}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
