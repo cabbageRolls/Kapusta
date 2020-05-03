27d2d55e59b89e817276f0748e3c658c33820794
OlDashkevych <dashkevich672@gmail.com>
2020-05-03 12:57:47 +0300

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
-import React from "react";
-import "../CSS/normilize.css";
-import "../CSS/index.css";
+import React from 'react';
+import '../CSS/normilize.css';
+import '../CSS/index.css';
+
+import ProjectTitle from './ProjectTitle/ProjectTitle.jsx';
+import AuthInput from './AuthInput/AuthInput';
+import Header from './Header/';
 
 function App() {
-  return <div> </div>;
+  return <div></div>;
 }
 
 export default App;
