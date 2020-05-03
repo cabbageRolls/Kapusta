972367967b99730fed147e72515da28205dc9489
sergiy khurtik <skhurtik@ukr.net>
2020-05-03 22:15:57 +0300

    fixed WelcomeNotification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import AdditionalButton from './AdditionalButton/AdditionalButton';
-import WelcomeNotification from './WelcomeNotification/WelcomeNotification';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
 
 function App() {
-  return (
-    <div>
-      <AdditionalButton />
-      <WelcomeNotification />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
