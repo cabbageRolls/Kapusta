d2b98768de9bf3ea6da0c211d4005d5c54981ecd
dmb-2222 <dmb-2222@yandex.ru>
2020-05-03 20:26:04 +0300

    balanceDetails done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-
+import BalanceDetails from '../components/BalanceDetails';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BalanceDetails />
+    </div>
+  );
 }
 
 export default App;
