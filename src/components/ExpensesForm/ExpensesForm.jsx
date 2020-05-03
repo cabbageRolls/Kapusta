f067df8b061115cde0f7016b359e316bf08685fe
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-03 23:46:49 +0300

    not mobile

--- src/components/ExpensesForm/ExpensesForm.jsx
+++ src/components/ExpensesForm/ExpensesForm.jsx
@@ -4,23 +4,27 @@ import styles from './ExpensesForm.module.css';
 
 export default function ExpensesForm() {
   return (
-    <form>
-      <input
-        type="text"
-        name="description"
-        placeholder="Здесь ты будешь вносить на что ты тратишь деньги"
+    <>
+      <form action="" className={styles.form}>
+        <input
+          className={styles.descriptionInput}
+          type="text"
+          name="description"
+          placeholder="Здесь ты будешь вносить на что ты тратишь деньги"
+          // value={name}
+          // onChange={this.handleChange}
+        />
 
-        // value={name}
-        // onChange={this.handleChange}
-      />
-
-      <input
-        type="text"
-        name="amount"
-        placeholder="0.00"
-        // value={amount}
-        // onChange={this.handleChange}
-      />
-    </form>
+        <input
+          className={styles.amountInput}
+          type="text"
+          name="amount"
+          placeholder="0.00"
+          // value={0.0}
+          // onChange={this.handleChange}
+        />
+        <div className={styles.iconCalculator}></div>
+      </form>
+    </>
   );
 }
