14b52758aa01547360b36f4bdb071eb2c87d2a46
Marina <marfen24@i.ua>
2020-05-04 11:25:59 +0300

    DeleteTrashButton done

---
+++ src/components/DeleteTrashButton/DeleteTrashButton.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Styles from './DeleteTrashButton.module.css';
+export default function DeleteTrashButton() {
+  return <button className={Styles.Button}></button>;
+}
