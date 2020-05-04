2bfc84d3522b7dff71ad4fc531523f2f06571b58
Alexanderqwer <llkkfi27456@gmail.com>
2020-05-04 18:28:58 +0300

    added MonthPicker

--- src/components/MonthPicker/index.jsx
+++ src/components/MonthPicker/index.jsx
@@ -1,23 +1,26 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useMediaQuery } from 'react-responsive';
 import Styles from './MonthPicker.module.css';
 
-const index = () => {
+const index = ({ month, year, increment, decrement }) => {
   const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
   return (
     <div className={Styles.container}>
       {isMobile ? '' : <h4 className={Styles.title}>Текущий период:</h4>}
-      <div>
-        <button className={Styles.button}>
+      <div className={Styles.wrapper}>
+        <button className={Styles.button} onClick={decrement}>
           <img
-            className={Styles.svg}
+            className={[Styles.svg, Styles.svgLeft].join(' ')}
             src="./images/svg/arrow_right-24px.svg"
             alt="arrowLeft"
           />
         </button>
-        <span className={Styles.data}>Ноябрь 2019</span>
-        <button className={Styles.button}>
+        <div className={Styles.data}>
+          {month} {year}
+        </div>
+        <button className={Styles.button} onClick={increment}>
           <img
             className={Styles.svg}
             src="./images/svg/arrow_right-24px.svg"
@@ -27,5 +30,11 @@ const index = () => {
       </div>
     </div>
   );
+};
+index.propTypes = {
+  month: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired,
 };
 export default index;
