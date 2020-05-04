4f0be821d0fa743daca7eed12a7c78eb57aa8ee9
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-04 15:07:48 +0300

    edit query

--- src/services/mediaQuery.js
+++ src/services/mediaQuery.js
@@ -8,10 +8,10 @@ export const Tablet = ({ children }) => {
   return isTablet ? children : null;
 };
 export const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 480 });
   return isMobile ? children : null;
 };
 export const Default = ({ children }) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 });
+  const isNotMobile = useMediaQuery({ minWidth: 480 });
   return isNotMobile ? children : null;
 };
