8f1e76fbc6363a6ed845c93c57c3f70de7930c20
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-05 19:00:24 +0300

    added ExpensesForm

--- src/components/ExpensesForm/ExpensesForm.jsx
+++ src/components/ExpensesForm/ExpensesForm.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+import React, { Component } from 'react';
 import styles from './ExpensesForm.module.css';
 
-export default function ExpensesForm() {
-  return (
-    <>
-      <form action="" className={styles.form}>
-        <input
-          className={styles.descriptionInput}
-          type="text"
-          name="description"
-          placeholder="Здесь ты будешь вносить на что ты тратишь деньги"
-          // value={name}
-          // onChange={this.handleChange}
-        />
+export default class ExpensesForm extends Component {
+  state = {
+    inputValue: '',
+  };
 
-        <input
-          className={styles.amountInput}
-          type="text"
-          name="amount"
-          placeholder="0.00"
-          // value={0.0}
-          // onChange={this.handleChange}
-        />
-        <div className={styles.iconCalculator}></div>
-      </form>
-    </>
-  );
+  handleChange = e => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { inputValue } = this.state;
+    const { onSubmit } = this.props;
+    onSubmit(inputValue);
+
+    this.setState({
+      inputValue: '',
+    });
+  };
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <>
+        <form action="" className={styles.form} onSubmit={this.handleSubmit}>
+          <div className={styles.div}>
+            <input
+              className={styles.descriptionInput}
+              type="text"
+              name="description"
+              value={inputValue}
+              onChange={this.handleChange}
+            />
+            {inputValue.length === 0 && (
+              <span className={styles.placeholder}>
+                Здесь ты будешь вносить на что ты тратишь деньги
+              </span>
+            )}
+          </div>
+          <input
+            className={styles.amountInput}
+            type="text"
+            name="amount"
+            placeholder="0.00"
+          />
+          <div className={styles.border}></div>
+          <div className={styles.iconCalculator}></div>
+        </form>
+      </>
+    );
+  }
 }
