c6e94ce0331e51da173233a0e0f4f9e666e0e800
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-07 12:55:37 +0300

    added IncomeForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,14 @@ import '../CSS/index.css';
 
 import ProjectTitle from './ProjectTitle/ProjectTitle.jsx';
 import AuthInput from './AuthInput/AuthInput';
+import IncomeForm from './IncomeForm/IncomeForm';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <IncomeForm />
+    </div>
+  );
 }
 
 export default App;
