12f7c17aedf461dd983aa7a3bd36a101e1962f37
dmb-2222 <dmb-2222@yandex.ru>
2020-05-10 15:06:03 +0300

    mainPge V.1

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import MainPge from '../pages/MainPage/'
+
 const Router = () => {
   return (
     <Switch>
-      <Route path="/" component={() => <div></div>} />
+      <Route path="/main-page" component={MainPge}/>
     </Switch>
   );
 };
