9bef712e5f21a281a5adc94a485ddf7a1d09608b
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-11 12:51:05 +0300

    fixed styles

--- src/pages/ExpensesPage/ExpensesPage.jsx
+++ src/pages/ExpensesPage/ExpensesPage.jsx
@@ -6,14 +6,29 @@ import ExpensesForm from '../../components/ExpensesForm/ExpensesForm';
 import ActionButton from '../../components/ActionButton/ActionButton';
 import ExpensesList from '../../components/ExpensesList/index';
 import MonthSummary from '../../components/MonthSummary/Index';
+import data from '../../components/MonthSummary/month.json';
 
 const ExpensesPage = () => (
-  <div className={styles.WelcomingPage}>
-    <DatePicker />
-    <ExpensesForm />
-    <ActionButton />
-    <ExpensesList />
-    <MonthSummary />
+  <div className={styles.Wrapper}>
+    <div className={styles.Form}>
+      <div className={styles.DatePicker}>
+        <DatePicker />
+      </div>
+      <div className={styles.ExpensesForm}>
+        <ExpensesForm />
+      </div>
+      <div className={styles.ActionButton}>
+        <ActionButton />
+      </div>
+    </div>
+    <div className={styles.ExpensesListContainer}>
+      <div className={styles.ExpensesList}>
+        <ExpensesList />
+      </div>
+      <div className={styles.MonthSummary}>
+        <MonthSummary data={data} />
+      </div>
+    </div>
   </div>
 );
 
