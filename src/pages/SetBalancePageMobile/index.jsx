274bef95cb9c80b37de09298ab098ce09147c9b1
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-11 23:37:17 +0300

    added SetBalanceForm mob tabl desk

--- src/pages/SetBalancePageMobile/index.jsx
+++ src/pages/SetBalancePageMobile/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Styles from './setBalancePageMobile.module.css';
-import SetBalanceForm from '../../components/setBalanceForm';
 
+import SetBalanceForm from '../../components/SetBalanceForm';
+
+// eslint-disable-next-line react/prefer-stateless-function
 export default class SetBalansePage extends React.Component {
   render() {
     return (
