b9edf3cd996c3643dfcb686736e88fd4083d8d12
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-11 23:44:33 +0300

    added SetBalanceForm mob tabl desk

--- src/pages/SetBalancePageMobile/index.jsx
+++ src/pages/SetBalancePageMobile/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Styles from './setBalancePageMobile.module.css';
-import SetBalanceForm from '../SetBalanceForm';
-// import { Mobile } from '../../services/mediaQuery';
+import SetBalanceForm from '../../components/SetBalanceForm';
 
 export default class SetBalansePage extends React.Component {
   render() {
