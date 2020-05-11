0e80ac7b00d1b2708955118135d3a454efc28b2a
sergiy khurtik <skhurtik@ukr.net>
2020-05-11 22:32:50 +0300

    deleted page SetIncomePageMobile from router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import SetIncomePageMobile from '../pages/SetIncomePageMobile/SetIncomePageMobile';
 import { Switch, Route } from 'react-router-dom';
 
 const Router = () => {
   return (
     <Switch>
-      <Route
-        path="/"
-        component={() => (
-          <div>
-            <SetIncomePageMobile />
-          </div>
-        )}
-      />
+      <Route path="/" component={() => <div></div>} />
     </Switch>
   );
 };
