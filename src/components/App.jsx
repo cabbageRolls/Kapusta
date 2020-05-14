781303a51819b9dd5dbfeefd5d33843498b1465c
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-14 13:39:06 +0300

    fixed logo component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-import SetBalancePage from '../pages/SetBalancePageMobile';
+import Logo from '../components/logo';
+import Loader from '../components/loader';
+import Chart from '../components/ExpensesChartBySpecificCategory';
 
 function App() {
   return (
     <div>
-      <SetBalancePage />
+      <Loader />
+      <Chart
+        data={[
+          { name: 'chery', cost: '2500' },
+          { name: 'bacon', cost: '4500' },
+          { name: 'tomato', cost: '500' },
+          { name: 'chery', cost: '5500' },
+          { name: 'bacon', cost: '4100' },
+          { name: 'tomato', cost: '300' },
+          { name: 'chery', cost: '2000' },
+          { name: 'bacon', cost: '1500' },
+          { name: 'tomato', cost: '500' },
+          { name: 'tomato', cost: '200' },
+        ]}
+      />
+      <Logo />
     </div>
   );
 }
