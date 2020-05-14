79fb78eeac0f02d30418d004293d6231f769c9c9
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-05-14 14:52:42 +0300

    clear app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,12 @@
-// import React from 'react';
-// import '../CSS/normilize.css';
-// import '../CSS/index.css';
-// import SetBalancePage from '../pages/SetBalancePageMobile';
-
-// function App() {
-//   return (
-//     <div>
-//       <SetBalancePage />
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-import Logo from '../components/logo';
-import Loader from '../components/loader';
-import Chart from '../components/ExpensesChartBySpecificCategory';
+import SetBalancePage from '../pages/SetBalancePageMobile';
 
 function App() {
   return (
     <div>
-      <Loader />
-      <Chart
-        data={[
-          { name: 'chery', cost: '2500' },
-          { name: 'bacon', cost: '4500' },
-          { name: 'tomato', cost: '500' },
-          { name: 'chery', cost: '5500' },
-          { name: 'bacon', cost: '4100' },
-          { name: 'tomato', cost: '300' },
-          { name: 'chery', cost: '2000' },
-          { name: 'bacon', cost: '1500' },
-          { name: 'tomato', cost: '500' },
-          { name: 'tomato', cost: '200' },
-        ]}
-      />
-      <Logo />
+      <SetBalancePage />
     </div>
   );
 }
