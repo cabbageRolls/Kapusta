895e28c2c5805781ce723e2a07f79d879fa0c72c
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-15 12:46:37 +0300

    fixed styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
+import IncomeForm from './IncomeForm/IncomeForm';
 
 function App() {
   return (
     <div>
+      <IncomeForm />
     </div>
   );
 }
--- src/components/IncomeForm/IncomeForm.jsx
+++ src/components/IncomeForm/IncomeForm.jsx
@@ -3,53 +3,58 @@ import styles from './IncomeForm.module.css';
 
 export default class IncomeForm extends Component {
   state = {
+    inputDescription: '',
     inputValue: '',
   };
 
   handleChange = e => {
     this.setState({
-      inputValue: e.target.value,
+      inputDescription: e.target.value,
     });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    const { inputValue } = this.state;
+    const { inputDescription } = this.state;
     const { onSubmit } = this.props;
-    onSubmit(inputValue);
+    onSubmit(inputDescription);
 
     this.setState({
-      inputValue: '',
+      inputDescription: '',
     });
   };
   render() {
-    const { inputValue } = this.state;
+    const { inputDescription } = this.state;
     return (
       <>
-        <form action="" className={styles.form} onSubmit={this.handleSubmit}>
-          <div className={styles.div}>
+        <div className={styles.container}>
+          <form action="" className={styles.form} onSubmit={this.handleSubmit}>
+            <div className={styles.div}>
+              <input
+                className={styles.descriptionInput}
+                type="text"
+                name="description"
+                value={inputDescription}
+                onChange={this.handleChange}
+              />
+              {inputDescription.length === 0 && (
+                <span className={styles.placeholder}>
+                  Поздравляю! Сколько ты заработал?
+                </span>
+              )}
+            </div>
             <input
-              className={styles.descriptionInput}
+              className={styles.amountInput}
               type="text"
-              name="description"
-              value={inputValue}
-              onChange={this.handleChange}
+              name="amount"
+              // value={inputValue}
+              placeholder="0.00"
+              maxLength="8"
             />
-            {inputValue.length === 0 && (
-              <span className={styles.placeholder}>
-                Поздравляю! Сколько ты заработал?
-              </span>
-            )}
-          </div>
-          <input
-            className={styles.amountInput}
-            type="text"
-            name="amount"
-            placeholder="0.00"
-          />
-          <div className={styles.border}></div>
-          <div className={styles.iconCalculator}></div>
-        </form>
+            <div className={styles.border}></div>
+            <div className={styles.iconCalculator}></div>
+          </form>
+        </div>
       </>
     );
   }
