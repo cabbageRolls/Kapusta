d29cfae49039899ade713994d578d81711ec6fe0
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-16 17:29:11 +0300

    fixed breakpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-import ProjectTitle from './ProjectTitle/ProjectTitle';
-import AuthForm from './AuthForm/AuthForm';
-import ExpensesForm from './ExpensesForm/ExpensesForm';
-import GoBackButton from './GoBackButton/GoBackButton';
-import ExpensesList from './ExpensesList/index';
 
 function App() {
-  return (
-    <div>
-      {/* <ProjectTitle />
-      <AuthForm /> */}
-      <ExpensesForm />
-      {/* <GoBackButton /> */}
-      <ExpensesList />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
