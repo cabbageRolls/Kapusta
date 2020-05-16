317998e59db4ee6efddc835279407936e692f87f
Alexanderqwer <llkkfi27456@gmail.com>
2020-05-16 11:42:07 +0300

    добавил медиа правила

--- src/components/MonthPicker/index.jsx
+++ src/components/MonthPicker/index.jsx
@@ -1,41 +1,35 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useMediaQuery } from 'react-responsive';
+import { Default } from '../../services/mediaQuery';
 import Styles from './MonthPicker.module.css';
-
-const index = ({ month, year, increment, decrement }) => {
-  const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+const Icon = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M10 17l5-5-5-5v10z" />
+    <path fill="none" d="M0 24V0h24v24H0z" />
+  </svg>
+);
+const index = ({ month = 'сентябрь', year = '2020', increment, decrement }) => {
   return (
     <div className={Styles.container}>
-      {isMobile ? '' : <h4 className={Styles.title}>Текущий период:</h4>}
+      <Default>
+        <h4 className={Styles.title}>Текущий период:</h4>
+      </Default>
       <div className={Styles.wrapper}>
         <button className={Styles.button} onClick={decrement}>
-          <svg
-            className={[Styles.svg, Styles.svgLeft].join(' ')}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 17l5-5-5-5v10z" />
-            <path fill="none" d="M0 24V0h24v24H0z" />
-          </svg>
+          <Icon className={Styles.svgLeft} />
         </button>
         <div className={Styles.data}>
           {month} {year}
         </div>
         <button className={Styles.button} onClick={increment}>
-          <svg
-            className={Styles.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 17l5-5-5-5v10z" />
-            <path fill="none" d="M0 24V0h24v24H0z" />
-          </svg>
+          <Icon className={Styles.svg} />
         </button>
       </div>
     </div>
