d87f6262febaf244410576b3411c90ee81b87c36
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-16 17:27:02 +0300

    fixed breakpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-import SetBalancePage from '../pages/SetBalancePageMobile';
 
 function App() {
-
   return <div></div>;
-
 }
 
 export default App;
