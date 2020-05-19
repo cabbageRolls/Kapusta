bac712712b9a6d7083b8337061fc8c6ddc9f5b9d
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-19 21:53:55 +0300

    fixes styles SetBalanceForm mobile and tablet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-<<<<<<< HEAD
 
 function App() {
   return <></>;
-=======
-import MainPage from '../pages/MainPage';
-
-function App() {
-  return (
-    <div>
-      <MainPage />
-    </div>
-  );
->>>>>>> added set balance form
 }
 
 export default App;
--- src/components/setBalanceForm/index.jsx
+++ src/components/setBalanceForm/index.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import MediaQuery from 'react-responsive';
 import Styles from './setBalanceForm.module.css';
 import DatePickerCustom from '../datePicker';
 import Notification from '../WelcomeNotification/WelcomeNotification';
-import AdditionalButton from '../AdditionalButton/AdditionalButton';
-import ReportButton from '../goToReportsButton';
-import ExpensesList from '../ExpensesList';
-import ExpensesForm from '../ExpensesForm/ExpensesForm';
-import ActionButton from '../ActionButton/ActionButton';
-import MonthSummary from '../MonthSummary';
-import Header from '../header';
 
 export default class setBalanceForm extends React.Component {
   state = {
@@ -43,85 +35,38 @@ export default class setBalanceForm extends React.Component {
     const { inputValue, balance } = this.state;
 
     return (
-      <div className={Styles.mainContainer}>
-        {/* HEADER */}
-        <Header />
-        <div className={Styles.container}>
-          <div className={Styles.balance_container}>
-            {/* BALANCE CONTAINER */}
-            <div className={Styles.container_reportButton}>
-              <ReportButton className={Styles.reportButton} active={balance} />
-            </div>
-            <p className={Styles.text}>Баланс:</p>
-            <form
-              onSubmit={this.handleSubmit}
-              action=""
-              className={Styles.form}
-            >
-              <input
-                className={Styles.input}
-                type="text"
-                value={inputValue}
-                onChange={this.handleChange}
-                placeholder={
-                  balance ? `${Number(balance).toFixed(2)} UAH` : '00.00 UAH'
-                }
-                pattern="\d+(\.\d{2})?"
-              />
-              <button className={Styles.button}>
-                <div className={Styles.containerBtn}>подтвердить</div>
-              </button>
-            </form>
-          </div>
+      <div className={Styles.container}>
+        <div className={Styles.balance_container}>
+          {/* BALANCE CONTAINER */}
 
-          <MediaQuery minDeviceWidth={768}>
-            <AdditionalButton active={balance} />
-          </MediaQuery>
-          <div className={Styles.container_expenses}>
-            <MediaQuery maxDeviceWidth={767}>
-              {balance ? <DatePickerCustom /> : <Notification />}
-            </MediaQuery>
-
-            <MediaQuery minDeviceWidth={768}>
-              {!balance && <Notification />}
-              <div className={Styles.datePickerExpensesForm_container}>
-                <DatePickerCustom />
-                <ExpensesForm />
-
-                <MediaQuery minDeviceWidth={991}>
-                  <div className={Styles.actionButton}>
-                    <ActionButton upButton={'Ввод'} inButton={'Очистить'} />
-                  </div>
-                </MediaQuery>
-              </div>
-            </MediaQuery>
-
-            <MediaQuery minDeviceWidth={768} and maxDeviceWidth={990}>
-              <div className={Styles.actionButton}>
-                <ActionButton upButton={'Ввод'} inButton={'Очистить'} />
-              </div>
-            </MediaQuery>
-
-            <div className={Styles.containerExpensesList}>
-              <ExpensesList />
-
-              <MediaQuery minDeviceWidth={991}>
-                <MonthSummary />
-              </MediaQuery>
-            </div>
-
-            <MediaQuery maxDeviceWidth={767}>
-              <AdditionalButton active={balance} />
-            </MediaQuery>
-
-            <MediaQuery minDeviceWidth={768} and maxDeviceWidth={990}>
-              <MonthSummary />
-            </MediaQuery>
-          </div>
+          <p className={Styles.text}>Баланс:</p>
+          <form onSubmit={this.handleSubmit} action="" className={Styles.form}>
+            <input
+              className={Styles.input}
+              type="text"
+              value={inputValue}
+              onChange={this.handleChange}
+              placeholder={
+                balance ? `${Number(balance).toFixed(2)} UAH` : '00.00 UAH'
+              }
+              pattern="\d+(\.\d{2})?"
+            />
+            <button type="submit" className={Styles.button}>
+              <div className={Styles.containerBtn}>подтвердить</div>
+            </button>
+          </form>
         </div>
+
+        {balance ? (
+          <div className={Styles.datePicker}>
+            <DatePickerCustom />
+          </div>
+        ) : (
+          <div className={Styles.notification}>
+            <Notification />
+          </div>
+        )}
       </div>
     );
   }
 }
-
-// REBASE
