edb1634245558c68012c50367f260aaf01174d34
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-21 18:20:51 +0300

    fixed styles GoogleAuthButton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-<<<<<<< HEAD
-
-=======
->>>>>>> b4a82a4505025a843b030e325fa6845c5fb44f9f
 import GoogleBtn from './googleAuthButton';
 
 function App() {
