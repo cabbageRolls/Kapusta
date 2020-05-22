521b5e80e6d5c2e0448e13ff5eb06e63d901be21
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-22 20:48:00 +0300

    fixed mediaquery

--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { isMobile, isTablet } from '../../services/mediaQuery';
 import styles from './GoBackButton.module.css';
 
 export default function GoBackButton() {
-  const isTablet = useMediaQuery({
-    query: '(max-width: 768px) and (min-width: 480px)',
-  });
-  const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+  const Mobile = isMobile(useMediaQuery);
+  const Tablet = isTablet(useMediaQuery);
   return (
     <div className={styles.divButton}>
       {' '}
       <button
         type="button"
         className={
-          isMobile
+          Mobile
             ? styles.GoBackButtonMobile
-            : isTablet
+            : Tablet
             ? styles.GoBackButtonTablet
             : styles.GoBackButton
         }
       >
         {' '}
-        <div className={styles.GoBackButtonImg}></div>{' '}
-        {isMobile ? '' : isTablet ? 'Вернуться' : 'Вернуться на главную'}{' '}
+        <div className={styles.GoBackButtonImg} />{' '}
+        {Mobile ? '' : Tablet ? 'Вернуться' : 'Вернуться на главную'}{' '}
       </button>{' '}
     </div>
   );
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import ExpensesPage from '../pages/ExpensesPage/ExpensesPage';
 
 import MainPage from '../pages/MainPage';
+import GoBackButton from '../components/GoBackButton/GoBackButton';
 
 const Router = () => {
   return (
     <Switch>
+      <GoBackButton />
       <Route path="/" exact component={MainPage} />
       <Route path="/main" exact component={MainPage} />
       <Route path="/expensespage" component={ExpensesPage} />
