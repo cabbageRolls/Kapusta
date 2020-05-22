6d22dd5354da85b352b7478dfbc98d55f15632f0
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-22 20:21:37 +0300

    fixed styles googleButton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
-import ExpensesList from './ExpensesList';
-import expensesData from './ExpensesList/expensesData.json';
 
 function App() {
-  return (
-    <div>
-      <ExpensesList expenses={expensesData} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
