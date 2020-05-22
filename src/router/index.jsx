2c46a2394432cb1dca69b1cb2a3e9f6b58326123
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-22 22:26:54 +0300

    fixed mediaquery

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import WelcomingPage from '../pages/WelcomingPage/WelcomingPage';
+import WelcomingPage from '../pages/WelcomingPage/WelcomingPage';
 import ExpensesPage from '../pages/ExpensesPage/ExpensesPage';
 
 import MainPage from '../pages/MainPage';
-import GoBackButton from '../components/GoBackButton/GoBackButton';
 
 const Router = () => {
   return (
     <Switch>
-      <GoBackButton />
-      <Route path="/" exact component={MainPage} />
+      <Route path="/" exact component={WelcomingPage} />
       <Route path="/main" exact component={MainPage} />
       <Route path="/expensespage" component={ExpensesPage} />
     </Switch>
