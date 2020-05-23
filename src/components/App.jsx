272c5d3090d61568b100b5376da7c6f2479abd1f
Vitaly Oryschuk <Leilyua@ukr.net>
2020-05-23 20:53:12 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import '../CSS/index.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import Router from '../router';
+import GoBackButton from './GoBackButton/GoBackButton';
 
 function App() {
   return (
--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { isMobile, isTablet } from '../../services/mediaQuery';
+import { Tablet, Desktop } from '../../services/mediaQuery';
 import styles from './GoBackButton.module.css';
 
 export default function GoBackButton() {
-  const Mobile = isMobile(useMediaQuery);
-  const Tablet = isTablet(useMediaQuery);
   return (
-    <div className={styles.divButton}>
-      {' '}
-      <button
-        type="button"
-        className={
-          Mobile
-            ? styles.GoBackButtonMobile
-            : Tablet
-            ? styles.GoBackButtonTablet
-            : styles.GoBackButton
-        }
-      >
-        {' '}
-        <div className={styles.GoBackButtonImg} />{' '}
-        {Mobile ? '' : Tablet ? 'На главную' : 'Вернуться на главную'}{' '}
-      </button>{' '}
-    </div>
+    <button type="button" className={styles.GoBackButton}>
+      <div className={styles.GoBackButtonImg} />
+      <Tablet>На главную</Tablet>
+      <Desktop>Вернуться на главную</Desktop>
+    </button>
   );
 }
