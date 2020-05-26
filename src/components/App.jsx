c6c01fcc10fba0049d15c417ae0e8b36557add4c
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-26 20:25:12 +0300

    fixed styles mediaQuery and added reactHook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
+import Balance from './SetBalanceForm';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Balance />
+    </>
+  );
 }
 
 export default App;
