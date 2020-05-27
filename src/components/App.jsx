f332113008b7696de3b57727eaf68da98392b96d
Mihail <41691809+Koallla@users.noreply.github.com>
2020-05-27 18:30:09 +0300

    fixed styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
+import List from './ExpensesList';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <List />
+    </>
+  );
 }
 
 export default App;
