792e0777860e9497787c050cf9011eeb0b677f54
Marina <marfen24@i.ua>
2020-05-30 20:10:18 +0300

    fixed TotalBalance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
+import Balance from './TotalBalanceInfo';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Balance />
+    </>
+  );
 }
 
 export default App;
