8e1755667bc6597767061f94bd1641fa09ca6084
OlDashkevych <dashkevich672@gmail.com>
2020-06-01 22:27:53 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
 
-import SetExpensesPageMobile from './Header';
-
 function App() {
   return (
-    <>
-      <SetExpensesPageMobile />
-    </>
+    <></>
   );
 }
 
