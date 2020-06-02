80bae603f49571d08e735c02ba28920d556ba886
dmb-2222 <dmb-2222@yandex.ru>
2020-06-02 20:37:58 +0300

    added responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../CSS/normilize.css';
 import '../CSS/index.css';
+import MainPage from '../pages/MainPage';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MainPage />
+    </>
+  );
 }
 
 export default App;
