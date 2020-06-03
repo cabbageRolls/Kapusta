64db8354787c2943843520beac39b44150b6cf1f
dmb-2222 <dmb-2222@yandex.ru>
2020-06-03 22:50:11 +0300

    add routes

---
+++ src/routes/routes.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const AUTH_PAGE = React.lazy(() =>
+  import('../pages/AuthPage' /* webpackChunkName: "AUTH_PAGE" */),
+);
+
+const MAIN_PAGE = React.lazy(() =>
+  import('../pages/MainPage' /* webpackChunkName: "MAIN_PAGE" */),
+);
+
+const REPORT_PAGE = React.lazy(() =>
+  import('../pages/ReportPage' /* webpackChunkName: "REPORT_PAGE" */),
+);
+
+const REPORT_PAGE = React.lazy(() =>
+  import('../pages/ReportPage' /* webpackChunkName: "REPORT_PAGE" */),
+);
+const SET_BALANCE_PAGE_MOBILE = React.lazy(() =>
+  import(
+    '../pages/SetBalancePageMobile' /* webpackChunkName: "SET_BALANCE_PAGE_MOBILE" */
+  ),
+);
+
+const SET_EXPENSES_PAGE_MOBILE = React.lazy(() =>
+  import(
+    '../pages/SetExpensesPageMobile' /* webpackChunkName: "SET_EXPENSES_PAGE_MOBILE" */
+  ),
+);
+const SET_INCOME_PAGE_MOBILE = React.lazy(() =>
+  import(
+    '../pages/SetIncomePageMobile' /* webpackChunkName: "SET_INCOME_PAGE_MOBILE" */
+  ),
+);
+
+export default {
+  AUTH_PAGE: {
+    path: 'auth',
+    component: AUTH_PAGE,
+  },
+  MAIN_PAGE: {
+    path: '/', // redirect to exp_page
+    component: MAIN_PAGE,
+  },
+  REPORT_PAGE: {
+    path: '/report',
+    component: REPORT_PAGE,
+  },
+
+  SET_BALANCE_PAGE_MOBILE: {
+    path: '/balance',
+    component: SET_BALANCE_PAGE_MOBILE,
+  },
+  SET_EXPENSES_PAGE_MOBILE: {
+    path: '/expenses',
+    component: SET_EXPENSES_PAGE_MOBILE,
+  },
+  SET_INCOME_PAGE_MOBILE: {
+    path: '/income',
+    component: SET_INCOME_PAGE_MOBILE,
+  },
+};
