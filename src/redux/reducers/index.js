4fa43a3a6e4dc56879812bf0c29a878046826bdf
Saashen <saashen.kyiv@gmail.com>
2020-06-09 19:21:51 +0300

    redux-persist

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,22 @@
 import { combineReducers } from 'redux';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+
 import transactions from './transactions';
 import error from './error';
 import loader from './loader';
 import session from './session';
 
+const sessionPersistConfig = {
+  key: 'kapusta',
+  storage,
+  whitelist: ['token'],
+};
+
 const rootReducers = combineReducers({
   transactions,
   error,
   loader,
-  session,
+  session: persistReducer(sessionPersistConfig, session),
 });
 export default rootReducers;
