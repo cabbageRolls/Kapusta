01528d923b52710a38428a2aa9b837c42f75152a
Vitaly Oryschuk <Leilyua@ukr.net>
2020-07-05 13:02:15 +0300

    added mainPage to logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Styles from './index.module.css';
+import routes from '../../routes';
+
 export default function Loader() {
   return (
-    <Link to="/">
+    <Link to={routes.MAIN_PAGE.path}>
       <div className={Styles.logo}></div>
     </Link>
   );
