4047ceac0678130e766416d1dbeb18223ae74614
Saashen <saashen.kyiv@gmail.com>
2020-07-05 13:13:22 +0300

    final routing

--- src/components/GoBackButton/index.jsx
+++ src/components/GoBackButton/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Tablet, Desktop } from '../../services/mediaQuery';
+import { useMediaQuery } from 'react-responsive';
+import { isMobile } from '../../services/mediaQuery';
 import styles from './GoBackButton.module.css';
 import { Link } from 'react-router-dom';
 import routes from '../../routes';
 
 export default function GoBackButton() {
+  const Mobile = isMobile(useMediaQuery);
+
   return (
     <button type="button" className={styles.GoBackButton}>
-      <div className={styles.GoBackButtonImg} />
+      <Link
+        to={
+          Mobile
+            ? routes.SET_BALANCE_PAGE_MOBILE.path
+            : routes.EXPENSES.path
+        }
+      >
+        <div className={styles.GoBackButtonImg} />
+      </Link>
+
       <Link to={routes.EXPENSES.path}>
         <Tablet>На главную</Tablet>
         <Desktop>Вернуться на главную</Desktop>
