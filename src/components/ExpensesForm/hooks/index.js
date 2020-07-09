07177aeefe903d4b06ebf96e53c642e34fc90a69
Olexander Dashkevych <dashkevich672@gmail.com>
2020-07-09 13:50:25 +0300

    added animation

---
+++ src/components/ExpensesForm/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useInputChange } from './useInputChange';
+export { default as useIdChange } from './useIdChange';
+export { default as useDataChange } from './useDataChange';
+export { default as useVisibleChange } from './useVisibleChange';
